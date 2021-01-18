a55add3802627dfae5a3a33f4af449b1f51946b0
Andrew Miller <jazznerd206@icloud.com>
2021-01-18 12:46:53 -0800

    user can leave room from GUI

--- framework/api/views.py
+++ framework/api/views.py
@@ -5,6 +5,7 @@ from .serializers import CreateRoomSerializer
 from .models import Room
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.http import JsonResponse
 
 # Create your views here.
 class RoomView(generics.ListAPIView):
@@ -26,6 +27,26 @@ class GetRoom(APIView):
             return Response({'Room Not Found': 'Invalid Room Code.'}, status=status.HTTP_404_NOT_FOUND)
 
         return Response({'Bad Request': 'Code paramater not found in request'}, status=status.HTTP_400_BAD_REQUEST)
+
+class JoinRoom(APIView):
+    lookup_url_kwarg = 'code'
+
+    def post(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        code = request.data.get(self.lookup_url_kwarg)
+        if code != None:
+            room_result = Room.objects.filter(code=code)
+            if len(room_result) > 0:
+                room = room_result[0]
+                self.request.session['room_code'] = code
+                return Response({'message': 'Room Joined!'}, status=status.HTTP_200_OK)
+
+            return Response({'Bad Request': 'Invalid Room Code'}, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response({'Bad Request': 'Invalid post data, did not find a code key'}, status=status.HTTP_400_BAD_REQUEST)
+
 
 class CreateRoomView(APIView):
     serializer_class = CreateRoomSerializer
@@ -49,3 +70,26 @@ class CreateRoomView(APIView):
                 room.save
             
             return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
+
+class UserInRoom(APIView):
+    def get(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        data = {
+            'code': self.request.session.get('room_code')
+        }
+        return JsonResponse(data, status=status.HTTP_200_OK)
+
+
+class LeaveRoom(APIView):
+    def post(self, request, format=None):
+        if 'room_code' in self.request.session:
+            self.request.session.pop('room_code')
+            host_id = self.request.session.session_key
+            room_results = Room.objects.filter(host=host_id)
+            if len(room_results) > 0:
+                room = room_results[0]
+                room.delete()
+
+        return Response({'Message': 'Success'}, status=status.HTTP_200_OK)
--- framework/frontend/src/components/HomePage.js
+++ framework/frontend/src/components/HomePage.js
@@ -1,23 +1,113 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom'
-import CreateRoom from "./CreateRoom.js";
+import React, { Component } from "react";
 import JoinRoom from "./JoinRoom.js";
+import CreateRoom from "./CreateRoom.js";
 import Room from "./Room.js";
+import { Grid, Button, ButtonGroup, Typography } from "@material-ui/core";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
 
 export default class HomePage extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            roomCode: null,
+        };
+        this.clearRoomCode = this.clearRoomCode.bind(this);
     }
-    render() {
-        return (
-            <Router>
-                <Switch>
-                    <Route exact path="/"><p>THis iS tHe hOmePaGe</p></Route>
-                    <Route path="/newRoom" component={CreateRoom} />
-                    <Route path="/join" component={JoinRoom} />
-                    <Route path='/room/:roomCode' component={Room} />
-                </Switch>
-            </Router>
-        )
+
+    async componentDidMount() {
+        fetch("/api/user-in-room")
+        .then((response) => response.json())
+        .then((data) => {
+            this.setState({
+                roomCode: data.code,
+            });
+        });
     }
+
+    clearRoomCode() {
+        this.setState({
+          roomCode: null,
+        });
+    }
+
+  renderHomePage() {
+    return (
+      <Grid 
+        container 
+        spacing={3}
+    >
+        <Grid 
+            item 
+            xs={12} 
+            align="center"
+        >
+          <Typography 
+            variant="h3" 
+            compact="h3"
+        >
+            House Party
+          </Typography>
+        </Grid>
+        <Grid 
+            item xs={12} 
+            align="center"
+        >
+          <ButtonGroup 
+            disableElevation 
+            variant="contained" 
+            color="primary"
+        >
+            <Button 
+                color="primary" 
+                to="/join" 
+                component={Link}
+            >
+              Join a Room
+            </Button>
+            <Button 
+                color="secondary" 
+                to="/create" 
+                component={Link}
+            >
+              Create a Room
+            </Button>
+          </ButtonGroup>
+        </Grid>
+      </Grid>
+    );
+  }
+
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => {
+              return this.state.roomCode ? (
+                <Redirect to={`/room/${this.state.roomCode}`} />
+              ) : (
+                this.renderHomePage()
+              );
+            }}
+          />
+          <Route path="/join" component={JoinRoom} />
+          <Route path="/create" component={CreateRoom} />
+          <Route
+            path="/room/:roomCode"
+            render={(props) => {
+              return <Room {...props} leaveRoomCallback={this.clearRoomCode} />;
+            }}
+          />
+        </Switch>
+      </Router>
+    );
+  }
 }
