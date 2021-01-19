5b5dd5b7a55feb697b1417c7d12f108b4938830e
Andrew Miller <jazznerd206@icloud.com>
2021-01-19 10:31:55 -0800

    add utils.py, get user tokens

---
+++ framework/spotify/util.py
@@ -0,0 +1,18 @@
+from .models import SpotifyToken
+from django.utils import timezone
+from datetime import timedelta
+from .credentials import CLIENT_ID, CLIENT_SECRET
+from requests import post, put, get
+
+
+BASE_URL = "https://api.spotify.com/v1/me/"
+
+
+def get_user_tokens(session_id):
+    user_tokens = SpotifyToken.objects.filter(user=session_id)
+    print(user_tokens)
+    if user_tokens.exists():
+        return user_tokens[0]
+    else:
+        return None
+
