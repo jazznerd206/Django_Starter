4cbe92fc3a4ce4cc07c8f7b328092da744b48853
Andrew Miller <jazznerd206@icloud.com>
2021-01-21 14:26:36 -0800

    functional components can now open rom

--- framework/frontend/src/components/Room2.js
+++ framework/frontend/src/components/Room2.js
@@ -13,73 +13,100 @@ function Room2(props) {
     const roomCode = props.match.params.roomCode;
     // console.log(roomCode);
 
+    const authenticateSpotify = () => {
+      fetch("/spotify/is-authenticated")
+        .then((response) => response.json())
+        .then((data) => {
+          console.log('status from spot: ' + data.status);
+          setSpotifyAuthenticated(data.status)
+          if (!data.status) {
+            fetch("/spotify/get-auth-url")
+              .then((response) => response.json())
+              .then((data) => {
+                window.location.replace(data.url);
+              });
+          }
+      });
+    }
+
+
     const getRoomDetails = () => {
-        return fetch("/api/get-room" + "?code=" + roomCode)
-          .then((response) => {
-            if (!response.ok) {
-              props.leaveRoomCallback;
-              props.history.push("/");
-            }
-            return response.json();
-          })
-          .then((data) => {
-            console.log(data);
-            if (isHost) {
-                console.log('authenticate spot here')
-            //   this.authenticateSpotify();
-            }
-          });
-        }
+      return fetch("/api/get-room" + "?code=" + roomCode)
+        .then((response) => {
+          if (!response.ok) {
+            props.leaveRoomCallback;
+            props.history.push("/");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          setVotesToSkip(data.votes_to_skip);
+          setIsHost(data.is_host);
+          setGuestCanPause(data.guest_can_pause);
+        });
+      }
     
     const getCurrentSong = () => {
-            fetch("/spotify/current-song")
-              .then((response) => {
-                if (!response.ok) {
-                  return {};
-                } else {
-                  return response.json();
-                }
-              })
-              .then((data) => {
-                  console.log(data)
-                setSong(data)
-              });
-            }
+      fetch("/spotify/current-song")
+        .then((response) => {
+          if (!response.ok) {
+            return {};
+          } else {
+            return response.json();
+          }
+        })
+        .then((data) => {
+            console.log(data)
+          setSong(data)
+        });
+      }
 
 
     const leaveButtonPressed = () => {
-        const requestOptions = {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-        };
-        fetch("/api/leave-room", requestOptions).then((_response) => {
-            props.leaveRoomCallback;
-            props.history.push("/");
-        });
+      const requestOptions = {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+      };
+      fetch("/api/leave-room", requestOptions).then((_response) => {
+          props.leaveRoomCallback;
+          props.history.push("/");
+      });
     }
 
     useEffect(() => {
-        getRoomDetails();
-        let interval = setInterval(getCurrentSong, 1000);
-        return () => {
-            clearInterval(interval);
-        }
+      getRoomDetails();
+      if (isHost) {
+        console.log('authenticate spot here')
+        authenticateSpotify();
+      }
+      let interval = setInterval(getCurrentSong, 1000);
+      return () => {
+          clearInterval(interval);
+      }
     }, [])
+
+    useEffect(() => {
+      if (isHost) {
+        console.log('authenticate spot here')
+        authenticateSpotify();
+      }
+    }, [isHost])
     
     return (
-        <div className="room-container">
-            <MusicPlayer {...song}/>
-            <Grid item xs={12} align="center">
-                <Button
-                    variant="contained"
-                    color="secondary"
-                    onClick={leaveButtonPressed}
-                >
-                    Leave Room
-                </Button>
-            </Grid>
-        </div>
+      <div className="room-container">
+          <MusicPlayer {...song}/>
+          <Grid item xs={12} align="center">
+              <Button
+                  variant="contained"
+                  color="secondary"
+                  onClick={leaveButtonPressed}
+              >
+                  Leave Room
+              </Button>
+          </Grid>
+      </div>
     )
-}
+  }
 
 export default Room2;
